fa9ff7bfa19f564d5dfd266b4639006966a6b91e
Danmo-Ar <danmoar.dev@proton.me>
2025-04-22 16:01:04 +0000

    feat(better-auth): add Api Request handle for better-auth

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,5 +5,4 @@
 import { $env } from '@/config/env';
 
 export const API_URL = $env.server.API_URL;
-export const NEXT_PUBLIC_API = $env.client.NEXT_PUBLIC_API;
 export const IS_CLIENT = typeof window !== 'undefined';
